6d697b8471ca6ebafd49cfa8220c653cb44050c7
Jay Harris <jay.harris@outlook.co.nz>
2021-05-17 20:51:36 +0200

    WIP book cover

--- src/book_cover.rs
+++ src/book_cover.rs
@@ -9,10 +9,18 @@ pub struct BookDetails {
 pub fn book_cover(ui: &mut egui::Ui, book: BookDetails) {
     let aspect_ratio = 2./3.;
     let height = 200.;
-    let desired_size = egui::vec2(height*aspect_ratio, height);
+    let width = aspect_ratio * height;
+    let desired_size = egui::vec2(width, height);
     
     let (rect, mut response) = ui.allocate_exact_size(desired_size, egui::Sense::click());
     let visuals = ui.style().interact(&response);
     let rect = rect.expand(visuals.expansion);
     ui.painter().rect(rect, visuals.corner_radius, visuals.bg_fill, visuals.bg_stroke);
+    ui.painter().rect_filled(
+        egui::Rect::from_min_size(
+            rect.left_top(), 
+            egui::vec2(width*book.progress, 10.)),
+        visuals.corner_radius,
+        egui::Color32::BLACK);
+    ui.label(book.title);
 }
