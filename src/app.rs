f0019309a697ed80470c3640ccecb8a69a0f3c4f
Jay Harris <jay.harris@outlook.co.nz>
2021-05-17 20:04:02 +0200

    Begin tweaking stuff

--- src/app.rs
+++ src/app.rs
@@ -2,27 +2,23 @@ use eframe::{egui, epi};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+pub struct BooksHome {
+    filter: String,
     value: f32,
-    painting: Painting,
 }
 
-impl Default for TemplateApp {
+impl Default for BooksHome {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
+            filter: "".to_owned(),
             value: 2.7,
-            painting: Default::default(),
         }
     }
 }
 
-impl epi::App for TemplateApp {
+impl epi::App for BooksHome {
     fn name(&self) -> &str {
-        "egui template"
+        "Books Home"
     }
 
     /// Called by the framework to load old app state (if any).
@@ -40,78 +36,20 @@ impl epi::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
-        let TemplateApp {
-            label,
+        let BooksHome {
+            filter,
             value,
-            painting,
         } = self;
 
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
-        egui::SidePanel::left("side_panel", 200.0).show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), |ui| {
-                ui.add(
-                    egui::Hyperlink::new("https://github.com/emilk/egui/").text("powered by egui"),
-                );
-            });
-        });
-
-        egui::TopPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                egui::menu::menu(ui, "File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        frame.quit();
-                    }
-                });
-            });
-        });
-
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("egui template");
-            ui.hyperlink("https://github.com/emilk/egui_template");
-            ui.add(egui::github_link_file_line!(
-                "https://github.com/emilk/egui_template/blob/master/",
-                "Direct link to source code."
-            ));
-            egui::warn_if_debug_build(ui);
-
+            ui.heading("Books");
             ui.separator();
-
-            ui.heading("Central Panel");
-            ui.label("The central panel the region left after adding TopPanel's and SidePanel's");
-            ui.label("It is often a great place for big things, like drawings:");
-
-            ui.heading("Draw with your mouse to paint:");
-            painting.ui_control(ui);
-            egui::Frame::dark_canvas(ui.style()).show(ui, |ui| {
-                painting.ui_content(ui);
+            ui.horizontal(|ui| {
+                ui.label("Filter books");
+                ui.text_edit_singleline(filter);
             });
+            ui.separator();
         });
-
-        if true {
-            egui::Window::new("Window").min_width(500.).collapsible(false).show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
-        }
     }
 }
 
